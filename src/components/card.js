b330677454fa96559ae1a61ef910cd1cc6448007
Brett Bloxom <brettbloxom@gmail.com>
2019-09-11 20:40:50 -0600

    functioning carousel

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Carousel from 'react-bootstrap/Carousel'
 
 import '../styles/card.css'
 
 const Card = () => {
-  return <Carousel.Item className="card">image here</Carousel.Item>
+  return <div>hi</div>
 }
 
 export default Card
--- src/components/cards.js
+++ src/components/cards.js
@@ -1,15 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'gatsby'
-import { Carousel } from 'react-materialize'
+import { Helmet } from 'react-helmet'
 
 import Card from './card'
 import '../styles/cards.css'
+import '../styles/card.css'
+import M from 'materialize-css'
 
 const Cards = () => {
-  const images = []
+  useEffect(() => {
+    const elems = document.querySelectorAll('.carousel')
+    M.Carousel.init(elems, options)
+  })
+
+  const options = {
+    dist: 0,
+    padding: 30,
+    numVisible: 3,
+    shift: 0,
+  }
+
   return (
-    <div className="cards">
-      <Carousel images={[1, 2, 3, 4, 5]} />
+    <div>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+        ></link>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        >
+      </Helmet>
+      <div className="carousel cards">
+        <div className="carousel-item card-image">
+          <a href="#one">one</a>
+        </div>
+        <div className="carousel-item card-image">
+          <a href="#one">two</a>
+        </div>
+        <div className="carousel-item card-image">
+          <a href="#one">three</a>
+        </div>
+        <div className="carousel-item card-image">
+          <a href="#one">four</a>
+        </div>
+      </div>
     </div>
   )
 }
