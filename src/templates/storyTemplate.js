09d7aadcdb706403efa183d2e7d2c8390f7274eb
Brett Bloxom <brettbloxom@gmail.com>
2019-09-13 12:16:33 -0600

    added gatsby-node

---
+++ src/templates/storyTemplate.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const Story = () => (
+  <Layout>
+    <SEO title="Story" />
+  </Layout>
+)
+
+export default Story
