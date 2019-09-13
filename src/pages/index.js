4cae7c58a22f369f00ba4b5e99100a619909fcb3
Brett Bloxom <brettbloxom@gmail.com>
2019-09-13 17:52:22 -0600

    swapped out homepage bg image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -9,15 +11,33 @@ import Social from '../components/social'
 
 import '../styles/index.css'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Social />
-    <div className="site">
-      <Main />
-      <Slider />
-    </div>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "home-bg-image.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1800) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  const bgImage = data.placeholderImage.childImageSharp.fluid
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Img fluid={bgImage} className="index-bg-image" />
+      <div className="index-image-overlay"></div>
+      <Social />
+      <div className="site">
+        <Main />
+        <Slider />
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
