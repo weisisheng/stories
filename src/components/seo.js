63cb9f76368bd009a26d591386ff92efe9c8a286
Brett Bloxom <brettbloxom@gmail.com>
2019-09-19 17:20:07 -0600

    met minimal ui

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,10 +5,10 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
@@ -35,6 +35,10 @@ function SEO({ description, lang, meta, title }) {
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
+        {
+          name: `viewport`,
+          content: `minimal-ui`,
+        },
         {
           name: `description`,
           content: metaDescription,
