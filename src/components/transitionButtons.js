d136efccd899b1cfac48bbad3199daf5d4e6b6b7
Brett Bloxom <brettbloxom@gmail.com>
2019-09-23 13:52:53 -0600

    fixing font awesome firefox

--- src/components/transitionButtons.js
+++ src/components/transitionButtons.js
@@ -5,6 +5,7 @@ import {
   faChevronLeft,
   faChevronRight,
 } from '@fortawesome/free-solid-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 import '../styles/transition-buttons.css'
 
