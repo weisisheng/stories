8dd0c46edbadb404d85e9814d57b713fdf52c067
Brett Bloxom <brettbloxom@gmail.com>
2019-09-27 11:58:12 -0600

    toggle mute for autoplay on small screens

--- src/templates/videoTemplate.js
+++ src/templates/videoTemplate.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { graphql, Link } from 'gatsby'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
@@ -10,6 +10,7 @@ import '../styles/video.css'
 
 export default ({ data }) => {
   const story = data.markdownRemark
+  const screenSize = useWindowSize()
 
   const prevPath = story.frontmatter.name.toLowerCase()
 
@@ -19,13 +20,23 @@ export default ({ data }) => {
       <Link to={`/${prevPath}/`} className="video-back-button">
         <FontAwesomeIcon icon={faChevronLeft} />
       </Link>
+      {screenSize.width < 1024 ? 
       <video controls={true} preload="auto" autoPlay loop muted>
         <source
-          src={`${story.frontmatter.videoSourceURL}&controls=0&autoplay=1`}
+          src={story.frontmatter.videoSourceURL}
           type="video/mp4"
         />
         Your browser does not support the video tag.
       </video>
+      :
+      <video controls={true} preload="auto" autoPlay loop>
+        <source
+          src={story.frontmatter.videoSourceURL}
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>}
+      
       <Social display="block" />
     </div>
   )
@@ -43,3 +54,31 @@ export const query = graphql`
     }
   }
 `
+
+function useWindowSize() {
+  const isClient = typeof window === 'object';
+
+  function getSize() {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined
+    };
+  }
+
+  const [windowSize, setWindowSize] = useState(getSize);
+
+  useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
+    
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize;
+}
